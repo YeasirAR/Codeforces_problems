91880158cca5863e564305ff9f02c0eb30844e74
yeasirar <yeasirar@gmail.com>
2021-10-16 10:18:31 +0600

    Add

---
+++ src/Team_231A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Team_231A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            if((sc.nextInt()+ sc.nextInt()+ sc.nextInt()) >=2){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
