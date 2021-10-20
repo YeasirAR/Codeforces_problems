dd4ae48bd9ddfc6a542ec8cb7c285aa0ff462653
yeasirar <yeasirar@gmail.com>
2021-10-20 08:47:03 +0600

    Add

---
+++ src/Domino_Piling_50A.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Domino_Piling_50A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println((m*n)/2);
+    }
+}
